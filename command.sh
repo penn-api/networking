9fbdf6c99d63a17176a876310dc3778f569b6e21
subh <subh@gmail.com>
2025-07-21 22:51:14 +0530

    lsof ss ifconfig.me ip a

--- command.sh
+++ command.sh
@@ -11,5 +11,8 @@ sudo ss -tulnp #to find port occupied.
 #172.16.0.0 - 172.31.255.255
 #192.168.0.0 - 192.168.255.255
 
-whois ip #using this we can check the aws backed ip easily
+whois ip #using this we can check the aws/cloudflare backed ip easily
 nslookup ip/dns #nameserver lookup to find the next hop
+tracert -4 aubank.in #to trace the dns (windows)
+traceroute -4 aubank.in #linux
+#what ever ip it returns if i do a whois ip then will know the provider protecting it.
