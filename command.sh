a2243e5cdec3e3e5f06ee8f68ad2e1c485b548f0
subh <subh@gmail.com>
2025-07-21 21:05:13 +0530

    lsof ss ifconfig.me ip a

---
+++ command.sh
@@ -0,0 +1,12 @@
+ip a #check local ip ---> private ip
+curl ifconfig.me #public ip
+curl -4 ifconfig.me
+ip route | grep default #router ip
+
+sudo lsof -i :80 #check service occupying port 80
+sudo ss -tulnp #to find port occupied.
+
+#private ip range 
+#10.0.0.0 - 10.255.255.255
+#172.16.0.0 - 172.31.255.255
+#192.168.0.0 - 192.168.255.255
